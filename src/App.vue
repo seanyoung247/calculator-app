89c7522d6f09305f70b2700536e6ff9c8deff553
seanyoung247 <seanyoung247@gmail.com>
2022-01-09 19:25:56 +0000

    feat: initial app setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <calculator msg="Caclulator App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import calculator from './components/calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    calculator
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // Prevents Vue from using absolute paths in github pages
+    publicPath: ''
+};
