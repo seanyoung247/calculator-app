f9df45e69e0b5e8bc836b62c148d3ce40457d5ab
seanyoung247 <seanyoung247@gmail.com>
2022-01-10 23:57:21 +0000

    feat: Adds initial theme switcher component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <h1>calc</h1>
+    <theme-toggler v-bind:themes=themes></theme-toggler>
   </header>
   <section>
     <screen></screen>
@@ -11,6 +12,7 @@
 </template>
 
 <script>
+  import themeToggler from './components/theme.vue';
   import screen from './components/screen.vue';
   import keypad from './components/keypad.vue';
   import './assets/global.css'
@@ -22,9 +24,13 @@
   export default {
     name: 'App',
     components: {
+      themeToggler,
       screen,
       keypad
     },
+    data() { return {
+      themes: themes
+    }},
     mounted() {
       setTheme(themes[0]);
     }
@@ -36,5 +42,10 @@
 </script>
 
 <style>
-
+  header {
+    display: flex;
+    width: 100%;
+    border: 1px solid red;
+    justify-content: space-between;
+  }
 </style>
---
+++ src/components/theme.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: 'theme-toggler',
+    props: {
+      themes: Array
+    }
+  }
+</script>
