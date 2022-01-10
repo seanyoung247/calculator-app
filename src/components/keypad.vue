04e713443ee2971c9080cc75a58f88c52ed491a7
seanyoung247 <seanyoung247@gmail.com>
2022-01-10 18:34:24 +0000

    feat: Adds initial keypad styles

--- src/components/keypad.vue
+++ src/components/keypad.vue
@@ -1,20 +1,65 @@
 <template>
-  <div>
-    <button v-on:click="callback()">1</button>
+  <div id="key-container">
+    <button v-on:click="callback">1</button>
+    <button v-on:click="callback">2</button>
+    <button v-on:click="callback">3</button>
+    <button v-on:click="callback">4</button>
+    <button v-on:click="callback">5</button>
+    <button v-on:click="callback">6</button>
+    <button v-on:click="callback">7</button>
+    <button v-on:click="callback">8</button>
   </div>
 </template>
 
 <script>
+  // const keys = [
+
+  // ]
+
+
   export default {
     name: 'keypad',
-    data: function() {
-      return {
-        callback: callback
-      }
-    }
+    data() { return {
+      callback: callback
+    }}
   }
 
-  function callback() {
-    console.log("clicky");
+  function callback(e) {
+    console.log(e.target);
   }
 </script>
+
+<style scoped>
+  #key-container {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: var(--key-padding);
+
+    padding: var(--app-padding);
+    border-radius: var(--app-radius);
+    background-color: var(--keys-back);
+  }
+
+  .key {
+    width: var(--key-width);
+    height: var(--key-height);
+  }
+
+  #key-container > button {
+    width: var(--key-width);
+    height: var(--key-height);
+    place-self: center;
+    margin: 0 0 4px 0;
+    border: none;
+    border-radius: var(--app-radius);
+    box-shadow: 0 4px 0 0 var(--key-shadow);
+    background-color: var(--key-color);
+    font-size: 32px;
+    font-family: 'Spartan', sans-serif;
+    color: var(--key-text);
+  }
+  #key-container > button:active {
+    margin: 4px 0 0 0;
+    box-shadow: none;
+  }
+</style>
