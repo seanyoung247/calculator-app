09573eecd014e8a6e4c92f10dc3148d95b408474
seanyoung247 <seanyoung247@gmail.com>
2022-01-10 23:59:22 +0000

    feat: Adds key button styles

--- src/components/keypad.vue
+++ src/components/keypad.vue
@@ -64,22 +64,43 @@
   .key {
     width: var(--key-width);
     height: var(--key-height);
-  }
-
-  #key-container > button {
-    width: var(--key-width);
-    height: var(--key-height);
+    
     place-self: center;
     margin: 0 0 4px 0;
+
     border: none;
     border-radius: var(--app-radius);
+    font-family: 'Spartan', sans-serif;
+  }
+
+  .key.wide {
+    width: calc( (var(--key-width) * 2) + var(--key-padding) );
+    grid-column-end: span 2;
+  }
+
+  .key.num {
     box-shadow: 0 4px 0 0 var(--key-shadow);
     background-color: var(--key-color);
-    font-size: 32px;
-    font-family: 'Spartan', sans-serif;
+    font-size: 28px;
     color: var(--key-text);
   }
-  #key-container > button:active {
+
+  .key.del {
+    box-shadow: 0 4px 0 0 var(--del-shadow);
+    background-color: var(--del-color);
+    font-size: 16px;
+    color: var(--del-text);
+    text-transform: uppercase;
+  }
+
+  .key.eql {
+    box-shadow: 0 4px 0 0 var(--eql-shadow);
+    background-color: var(--eql-color);
+    font-size: 18px;
+    color: var(--eql-text);
+  }
+
+  .key:active {
     margin: 4px 0 0 0;
     box-shadow: none;
   }
