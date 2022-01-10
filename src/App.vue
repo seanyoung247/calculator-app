a2fa7d8f9ab6d873a038aa8e8cac7f4239d3aecf
seanyoung247 <seanyoung247@gmail.com>
2022-01-10 18:32:10 +0000

    feat: Adds theme setting to app

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,25 @@
 
 <script>
   import keypad from './components/keypad.vue';
+  import './assets/global.css'
+
+  const themes = [
+    'dark', 'light', 'high-contrast'
+  ]
+
   export default {
     name: 'App',
     components: {
+      screen,
       keypad
+    },
+    mounted() {
+      setTheme(themes[0]);
     }
+  }
+
+  function setTheme(name) {
+    document.body.classList.add(name);
   }
 </script>
 
