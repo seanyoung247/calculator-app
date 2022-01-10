ca26fea624f8144261634263841b8ea7fd61111e
seanyoung247 <seanyoung247@gmail.com>
2022-01-10 18:30:49 +0000

    feat: Moves global styles to global.css

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,5 @@
 </script>
 
 <style>
-  :root {
-    --mobile-width: 375px;
-    --desktop-width: 1440px;
-  }
 
-  @import './assets/themes.css';
-
-  @import url('https://fonts.googleapis.com/css2?family=Spartan:wght@700&display=swap');
-  #app {
-    font-family: 'Spartan', sans-serif;
-    font-size: 32px;
-    color: var(--body-text);
-  }
 </style>
