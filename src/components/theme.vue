31d2f545ec6c01fb1b282dbd9acd6d7ee57af641
seanyoung247 <seanyoung247@gmail.com>
2022-01-11 00:00:08 +0000

    feat: Adds initial template html to theme chooser

--- src/components/theme.vue
+++ src/components/theme.vue
@@ -1,4 +1,15 @@
 <template>
+    <fieldset class="theme-toggler">
+      <legend>Theme</legend>
+      <ul class="theme-list">
+        <li v-for="(theme, index) in themes" v-bind:key="index">
+          <label class="theme-opt" v-bind:for="theme">
+            <span>{{ index + 1 }}</span>
+            <span><input type="radio" name="themer" v-bind:id="theme"></span>
+          </label>
+        </li>
+      </ul>
+    </fieldset>
 </template>
 
 <script>
