61080a1fdd7bf1300aeaa9f0d64020f834b5590d
seanyoung247 <seanyoung247@gmail.com>
2022-01-12 22:43:37 +0000

    feat: initial text resize code for screen

--- src/components/screen.vue
+++ src/components/screen.vue
@@ -5,16 +5,28 @@
 </template>
 
 <script>
+  import { measureText, getFont } from '../modules/measuretext.js';
+
   export default {
     name: 'screen',
     props: {
       value: String
+    },
+    methods: {
+      sizeText() {
+
+      }
+    },
+    mounted() {
+      const el = this.$refs.screenOuter;
+      console.log(measureText(this.value, getFont(el)));
     }
   }
 </script>
 
 <style scoped>
-  #screen {
+  .screen-container {
+    --resize: 1;
     position: relative;
     background-color: var(--screen-back);
     border-radius: var(--app-radius);
@@ -23,13 +35,14 @@
     font-size: var(--screen-font);
     overflow: hidden;
   }
-  #screen > span {
+  .screen {
     display: flex;
     justify-content: right;
     align-items: center;
     position: absolute;
     right: 0;
-    width: 100%;
+    min-width: 100%;
     height: 100%;
+    font-size: calc(var(--screen-font) * var(--resize));
   }
 </style>
