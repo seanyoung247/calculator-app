7f13899ff1e810156e59c56a1745cb39071c7174
seanyoung247 <seanyoung247@gmail.com>
2022-01-14 19:10:13 +0000

    feat: Simplifies screen text sizing

--- src/components/screen.vue
+++ src/components/screen.vue
@@ -1,56 +1,53 @@
 
 <template>
-  <div class="screen-container" ref="screenOuter">
-    {{value}}
-    <!-- <div class="screen" :style="sizeText()" ref="screenInner">{{ value }}</div> -->
+  <div class="screen-container" ref="screen">
+    <!-- {{value}} -->
+    <div class="screen">{{ value }}</div>
   </div>
 </template>
 
 <script>
   import { measureText, getFont } from '../modules/measuretext.js';
 
+  const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
   export default {
     name: 'screen',
     props: {
       value: String
     },
+    data() { return {
+      textRatio: 1
+    }},
     methods: {
       sizeText() {
-        const [outer, inner] = [this.$refs.screenOuter, this.$refs.screenInner];
+        const screen = this.$refs.screen;
 
-        if (outer && inner) {
-          const screenWidth = outer.clientWidth || 1;
-          const textWidth = measureText(this.value, outer);
-          if (textWidth > screenWidth) {
-            const ratio = Math.max(screenWidth / textWidth, 0.5);
-            return `--resize: ${ratio};`;
-          }
+        if (screen) {
+          this.textRatio = clamp( (screen.clientWidth / measureText(this.value, getFont(screen))), 0.5, 1.0 );
+        } else {
+          this.textRatio = 1;
         }
-
-        return "--resize: 1;";
       }
     },
-    mounted() {
-      const el = this.$refs.screenOuter;
-      console.log(measureText(this.value, getFont(el)));
+    watch: {
+      value() { this.sizeText(); }
     }
   }
 </script>
 
 <style scoped>
   .screen-container {
-    --resize: 1;
-
     height: var(--screen-height);
 
     text-align: right;
 
     background-color: var(--screen-back);
+    font-size: var(--screen-font);
 
     border-radius: var(--app-radius);
     border: var(--app-padding) solid var(--screen-back);
     border-bottom: none;
-    font-size: var(--screen-font);
 
     overflow-y: hidden;
     overflow-x: auto;
@@ -59,6 +56,6 @@
     height: 100%;
     min-width: 100%;
     text-align: right;
-    font-size: calc(var(--screen-font) * var(--resize));
+    font-size: calc(var(--screen-font) * v-bind(textRatio));
   }
 </style>
