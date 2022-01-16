9401d6990ea9dc03e73a2d1f3cb8f07f858c5ff3
seanyoung247 <seanyoung247@gmail.com>
2022-01-16 01:06:18 +0000

    feat: Adds mouse drag scrolling to screen

--- src/components/screen.vue
+++ src/components/screen.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="screen-container" ref="screen">
+  <div class="screen-container" @mousedown="dragStart" ref="screen">
     <div class="screen">{{ value }}</div>
   </div>
 </template>
@@ -17,6 +17,7 @@
     },
     data() { return {
       textRatio: 1,
+      scrollPos: {xPos: 0, x:0},
       ro: null
     }},
     methods: {
@@ -26,9 +27,27 @@
         if (screen) {
           // Ratio is maximum width divided by actual width clamped between 0.5 (half size) and 1 (full size)
           this.textRatio = clamp( (screen.clientWidth / measureText(this.value, getFont(screen))), 0.5, 1.0 );
+          // Try to keep 
         } else {
           this.textRatio = 1;
         }
+      },
+      dragStart(e) {
+        this.scrollPos = {
+          xPos: this.$refs.screen.scrollLeft,
+          x: e.clientX
+        };
+        document.addEventListener('mousemove', this.dragMove);
+        document.addEventListener('mouseup', this.dragEnd);
+      },
+      dragMove(e) {
+        const dX = e.clientX - this.scrollPos.x;
+        this.$refs.screen.scrollLeft = -(this.scrollPos.xPos - dX);
+
+      },
+      dragEnd() {
+        document.removeEventListener('mousemove', this.dragMove);
+        document.removeEventListener('mouseup', this.dragEnd);
       }
     },
     mounted() {
@@ -48,6 +67,8 @@
 
 <style scoped>
   .screen-container {
+    display: flex;
+    flex-direction: row-reverse;
     height: var(--screen-height);
 
     text-align: right;
@@ -58,14 +79,19 @@
     border-radius: var(--app-radius);
     border: var(--app-padding) solid var(--screen-back);
     border-bottom: none;
+    padding-bottom: var(--app-padding);
 
-    overflow-y: hidden;
-    overflow-x: auto;
+    overflow: hidden;
+    /* overflow-y: hidden;
+    overflow-x: auto; */
   }
   .screen {
+    display: inline-flex;
+    justify-content: end;
+    align-items: center;
+
     height: 100%;
-    min-width: 100%;
-    text-align: right;
+    min-width: 100%; 
     font-size: calc(var(--screen-font) * v-bind(textRatio));
   }
 </style>
